bb0d52733dd1b234c88137103d2c4461951efbf8
konstPialov <azure_dream@mail.ru>
2018-12-05 21:57:21 +0300

    dist added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import avg from './some.js';
+import avg from './modules/some.js';
+const first = require('./modules/first');
 
 console.log('hello');
 console.log(avg(1, 4, 8));
+first('text from first-module');
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,17 @@ let conf = {
         filename: 'main.js',
         publicPath: 'dist/'
     },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
     devServer: {
         overlay: true
     }
